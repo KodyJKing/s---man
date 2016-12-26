b51b9c83ebd2ae2ee81dc5dc6db968fd8eb69146
Pyrometheus <orion.nye@gmail.com>
2016-12-26 15:53:23 -0800

    Turrets aim!
    Turrets aim now but dont fire. added a turret prefab as well.

--- Assets/scripts/Turret.cs
+++ Assets/scripts/Turret.cs
@@ -2,8 +2,18 @@
 using System.Collections;
 
 public class Turret : MonoBehaviour {
+    public float angle;
+    public GameObject target;
     // Update is called once per frame
     void Update()
     {
+        Vector3 myPosition = this.transform.position;
+        Vector3 targetPosition = target.transform.position;
+        Vector3 direction = (targetPosition - myPosition).normalized;
+        angle = 180 - Vector3.Angle(new Vector3(1, 0, 0), direction);
+        if (targetPosition.y > myPosition.y)
+            angle *= -1;
+
+        this.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
     }
 }
